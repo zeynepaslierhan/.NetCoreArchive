957cc51f044d611ce6e8ab05d6d96304e747a01b
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-12-02 09:46:15 +0300

    PartialViews oluşturuldu. PartialView olluşturma gösterildi

---
+++ PartialViews/PartialViewPractices/PartialViewPractices/Controllers/Create2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewPractices.Controllers
+{
+    public class Create2 : Controller
+    {
+        public PartialViewResult PartialView_Index()
+        {
+            return PartialView();
+        }
+    }
+}
